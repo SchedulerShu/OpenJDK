86a0dc1727a412335e0eecd1f03c0f507610de2f
Shu F <shufu.shu@gmail.com>
2017-11-02 18:27:23 +0800

    blockingqueue

--- jdk/src/share/classes/java/util/concurrent/BlockingQueue.java
+++ jdk/src/share/classes/java/util/concurrent/BlockingQueue.java
@@ -391,4 +391,12 @@ public interface BlockingQueue<E> extends Queue<E> {
 
     Java 中提供了 7 种 BlockingQueue 的实现，在看线程池之前我根本搞不清楚究竟选择哪个，
     直到完整地对比总结以后，发现其实也没什么复杂。
+    ArrayBlockingQueue ：一个由数组结构组成的有界阻塞队列。
+	LinkedBlockingQueue ：一个由链表结构组成的有界阻塞队列。
+	PriorityBlockingQueue ：一个支持优先级排序的无界阻塞队列。
+	DelayQueue：一个使用优先级队列实现的无界阻塞队列。
+	SynchronousQueue：一个不存储元素的阻塞队列。
+	LinkedTransferQueue：一个由链表结构组成的无界阻塞队列。
+	LinkedBlockingDeque：一个由链表结构组成的双向阻塞队列。
+    
  */
